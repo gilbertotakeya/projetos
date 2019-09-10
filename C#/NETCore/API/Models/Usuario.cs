6cc2675f635833d65c22f7a8d209d65c470d28c5
gilberto <gilbertotakeya@yahoo.com.br>
2019-09-10 08:19:03 -0300

    Conectando no MYSQL e consumindo API com o POSTMAN

--- C#/NETCore/API/Models/Usuario.cs
+++ C#/NETCore/API/Models/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,15 @@ namespace API.Models
 {
     public class Usuario
     {
+        [Key]
         public int IdUsuario { get; set; }
         public string Nome { get; set; }
+        [DataType(DataType.Password)]
         public string Senha { get; set; }
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Telefone em formato inválido.")]
         public string Telefone { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail em formato inválido.")]
         public string Email { get; set; }
         public DateTime dataNascimento { get; set; }
         public string Endereco { get; set; }
